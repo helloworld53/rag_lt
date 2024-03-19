0e7a46598d57e26d42aad307a27aa1beda472013
Karan <kk53@users.noreply.huggingface.co>
2024-03-19 09:19:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -21,17 +21,26 @@ def load_model():
     model = Llama(model_path, embedding=True)
 
     st.success("Loaded NLP model from Hugging Face!")  # 👈 Show a success message
-    
-    
-    # pc = Pinecone(api_key=api_key)
-    # index = pc.Index("law")
-#     model_2_name = "TheBloke/zephyr-7B-beta-GGUF"
-#     model_2base_name = "zephyr-7b-beta.Q4_K_M.gguf"
-#     model_path_model = hf_hub_download(
-#     repo_id=model_2_name,
-#     filename=model_2base_name,
-#     cache_dir= '/content/models' # Directory for the model
-# )
+
+    model_2_name = "TheBloke/zephyr-7B-beta-GGUF"
+    model_2base_name = "zephyr-7b-beta.Q4_K_M.gguf"
+    model_path_model = hf_hub_download(
+    repo_id=model_2_name,
+    filename=model_2base_name,
+    cache_dir= '/content/models' # Directory for the model
+)   
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+    llm = LlamaCpp(
+    model_path=model_path_model,
+    temperature=0.75,
+    max_tokens=2500,
+    top_p=1,
+    callback_manager=callback_manager,
+    verbose=True, 
+    n_ctx=2048,
+    n_threads = 2# Verbose is required to pass to the callback manager
+)
+    st.success("loaded the second NLP model from Hugging Face!") 
 #     prompt_template = "<|system|>\
 # </s>\
 # <|user|>\
@@ -39,26 +48,22 @@ def load_model():
 # <|assistant|>"
 #     template = prompt_template
 #     prompt = PromptTemplate.from_template(template)
-#     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-#     llm = LlamaCpp(
-#     model_path=model_path_model,
-#     temperature=0.75,
-#     max_tokens=2500,
-#     top_p=1,
-#     callback_manager=callback_manager,
-#     verbose=True, 
-#     n_ctx=2048,
-#     n_threads = 2# Verbose is required to pass to the callback manager
-# )
-    return model
+
+    return model, llm
     
 st.title("Please ask your question on Lithuanian rules for foreigners.")
-a  = load_model()
+model,llm  = load_model()
+pc = Pinecone(api_key=apikeys)
+index = pc.Index("law")
 question = st.text_input("Enter your question:")
-# if question:
-#     # Perform Question Answering
-#     answer = qa_chain(context=context, question=question)
-    
-#     # Display the answer
-#     st.header("Answer:")
-#     st.write(answer)
+query = model.create_embedding(question)
+q = query['data'][0]['embedding']
+response = index.query(
+  vector=q,
+  top_k=1,
+  include_metadata = True,
+  namespace = "ns1"
+)
+response_t = response['matches'][0]['metadata']['text']
+st.header("Answer:")
+st.write(response_t)
