07722925430f672447b678104637a538e10120a4
helloworld53 <karankarnik904@gmail.com>
2024-03-19 17:56:57 +0200

    no clue

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def load_model():
     model_path = hf_hub_download(
     repo_id=model_name_or_path,
     filename=model_basename,
-    cache_dir= '/content/models' # Directory for the model
+    #cache_dir= '/content/models' # Directory for the model
 )
     model = Llama(model_path, embedding=True)
 
@@ -30,7 +30,7 @@ def load_model():
     model_path_model = hf_hub_download(
     repo_id=model_2_name,
     filename=model_2base_name,
-    cache_dir= '/content/models' # Directory for the model
+    #cache_dir= '/content/models' # Directory for the model
 )   
     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
     llm = LlamaCpp(
@@ -45,25 +45,7 @@ def load_model():
 )
     st.success("loaded the second NLP model from Hugging Face!") 
 
-    model_2_name = "TheBloke/zephyr-7B-beta-GGUF"
-    model_2base_name = "zephyr-7b-beta.Q4_K_M.gguf"
-    model_path_model = hf_hub_download(
-    repo_id=model_2_name,
-    filename=model_2base_name,
-    cache_dir= '/content/models' # Directory for the model
-)   
-    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-    llm = LlamaCpp(
-    model_path=model_path_model,
-    temperature=0.75,
-    max_tokens=2500,
-    top_p=1,
-    callback_manager=callback_manager,
-    verbose=True, 
-    n_ctx=2048,
-    n_threads = 2# Verbose is required to pass to the callback manager
-)
-    st.success("loaded the second NLP model from Hugging Face!") 
+ 
 #     prompt_template = "<|system|>\
 # </s>\
 # <|user|>\
@@ -73,39 +55,14 @@ def load_model():
 #     prompt = PromptTemplate.from_template(template)
 
     return model, llm
-#     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-#     llm = LlamaCpp(
-#     model_path=model_path_model,
-#     temperature=0.75,
-#     max_tokens=2500,
-#     top_p=1,
-#     callback_manager=callback_manager,
-#     verbose=True, 
-#     n_ctx=2048,
-#     n_threads = 2# Verbose is required to pass to the callback manager
-# )
-    return model, llm
+
     
 st.title("Please ask your question on Lithuanian rules for foreigners.")
 model,llm  = load_model()
-pc = Pinecone(api_key=apikeys)
-index = pc.Index("law")
-model,llm  = load_model()
-pc = Pinecone(api_key="003117b0-6caf-4de4-adf9-cc49da6587e6")
+apikey = st.secrets["apikey"] 
+pc = Pinecone(api_key=apikey)
 index = pc.Index("law")
 question = st.text_input("Enter your question:")
-query = model.create_embedding(question)
-q = query['data'][0]['embedding']
-response = index.query(
-  vector=q,
-  top_k=1,
-  include_metadata = True,
-  namespace = "ns1"
-)
-response_t = response['matches'][0]['metadata']['text']
-st.header("Answer:")
-st.write(response_t)
-
 query = model.create_embedding(question)
 q = query['data'][0]['embedding']
 response = index.query(
